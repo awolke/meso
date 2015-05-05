6757650c4511985dcb732cd92cb04d819f1efa68
Swiss Cryptotech <github@cryptote.ch>
2015-05-05 12:12:26 +0200

    Create slave_ubuntu.sh
    This is the first push of the Slave Installer File for Meso on Ununtu Server 14.04

---
+++ slave_ubuntu.sh
@@ -0,0 +1,13 @@
+# This is the Setup Script for the Ubuntu Slave Node -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]') CODENAME=$(lsb_release -cs)
+
+# Add the repository
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+# Lets Install Mesos
+sudo apt-get -y install mesos
+
+# For the Slave-Nodes we don't Need Zookeeper
+sudo service zookeeper stop
+sudo sh -c "echo manual > /etc/init/zookeeper.override"
