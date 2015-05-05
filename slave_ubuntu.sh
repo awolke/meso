abe80a6a479e1f629634518cd17c18f17f1dc3e3
Swiss Cryptotech <github@cryptote.ch>
2015-05-05 12:21:25 +0200

    fixed: Error in Distro Check

--- slave_ubuntu.sh
+++ slave_ubuntu.sh
@@ -1,5 +1,7 @@
 # This is the Setup Script for the Ubuntu Slave Node -
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]') CODENAME=$(lsb_release -cs)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
 
 # Add the repository
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
